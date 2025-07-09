dd45dad4618a66c298c6211903374a4386cf2f7d
Amit Thakur <amitthakur2915@gmail.com>
2025-07-09 12:36:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var errormessage = "";
-var missingfield = "";
 function isEmail(email) {
     var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     return regex.test(email);
 }
+
 $("#submitbutton").click(function () {
+    
+    var errormessage = "";
+    var missingfield = "";
+    $("#errors").html("");  
+    $("#success").html(""); 
+
     if ($("#Email").val() == "") {
         missingfield += "<p>Email not filled</p>";
     }
@@ -17,15 +22,16 @@ $("#submitbutton").click(function () {
 
     if (isEmail($("#Email").val()) == false) {
         errormessage += "<p>Email id is not valid</p>";
-    };
+    }
     if ($.isNumeric($("#phoneno").val()) == false) {
-        errormessage += "<p>Phone No.  is not valid</p>";
-    };
+        errormessage += "<p>Phone No. is not valid</p>";
+    }
     if ($("#password").val() != $("#confirmpassword").val()) {
         errormessage += "<p>Password does not match</p>";
     }
-    if (errormessage == "") {
-        $("#success").html("You are Registered")
+
+    if (errormessage == "" && missingfield == "") {
+        $("#success").html("You are Registered");
     } else {
         $("#errors").html(errormessage + missingfield);
     }
